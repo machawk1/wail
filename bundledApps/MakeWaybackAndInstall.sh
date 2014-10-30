79f1c92f1de324bb803d403196f197a516d3b56b
Mat Kelly <machawk1@gmail.com>
2014-10-30 09:22:49 -0400

    Updated unused MakeWayback directory to point to the correct location of tomcat.

--- bundledApps/MakeWaybackAndInstall.sh
+++ bundledApps/MakeWaybackAndInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set context
-cd /Applications/WAIL/tomcat/webapps/wayback-src/wayback-master
+cd /Applications/WAIL/bundledApps/tomcat/webapps/wayback-src/wayback-master
 
 #make
 mvn validate /Applications/WAIL/tomcat/webapps/wayback-src/wayback-master
@@ -9,12 +9,12 @@ mvn compile
 mvn package
 
 #move
-cp -r /Applications/WAIL/tomcat/webapps/wayback-src/wayback-master/wayback-webapp/target/wayback-1.7.1-SNAPSHOT /Applications/WAIL/tomcat/webapps/
-rm -r /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/WEB-INF
-cp -r /Applications/WAIL/tomcat/webapps/wayback-1.6/WEB-INF /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/WEB-INF
-cp -r /Applications/WAIL/tomcat/webapps/ROOT/files1 /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/
+cp -r /Applications/WAIL/bundledApps/tomcat/webapps/wayback-src/wayback-master/wayback-webapp/target/wayback-1.7.1-SNAPSHOT /Applications/WAIL/tomcat/webapps/
+rm -r /Applications/WAIL/bundledApps/tomcat/webapps/wayback-1.7.1-SNAPSHOT/WEB-INF
+cp -r /Applications/WAIL/bundledApps/tomcat/webapps/wayback-1.6/WEB-INF /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/WEB-INF
+cp -r /Applications/WAIL/bundledApps/tomcat/webapps/ROOT/files1 /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/
 
 
 #install
-mv /Applications/WAIL/tomcat/webapps/ROOT /Applications/WAIL/tomcat/webapps/ROOT_old
-mv /Applications/WAIL/tomcat/webapps/wayback-1.7.1-SNAPSHOT/ /Applications/WAIL/tomcat/webapps/ROOT/
+mv /Applications/WAIL/bundledApps/tomcat/webapps/ROOT /Applications/WAIL/tomcat/webapps/ROOT_old
+mv /Applications/WAIL/bundledApps/tomcat/webapps/wayback-1.7.1-SNAPSHOT/ /Applications/WAIL/tomcat/webapps/ROOT/
