7e1453e4cad7db5ad34c8c11b91fca5cfcf0e296
Mat Kelly <machawk1@gmail.com>
2015-03-03 17:07:05 -0500

    Fixed issue in built process wherein a previous instance in the /Applications/ folder on OS X would prevent a new version from being installed. Fixes #130

--- bundledApps/MAKEFILE.sh
+++ bundledApps/MAKEFILE.sh
@@ -8,18 +8,35 @@
 # You need python to execute it
 
 #if [ ! -z "$1" -a "$1" = "clean"  ]; then
-#  if [ -e tomcat/webapps/ROOT/file-db ]; then 
+#  if [ -e tomcat/webapps/ROOT/file-db ]; then
 #    rm -r tomcat/webapps/ROOT/file-db
 #  fi
-#  if [ -e tomcat/webapps/ROOT/index ]; then 
+#  if [ -e tomcat/webapps/ROOT/index ]; then
 #   rm -r tomcat/webapps/ROOT/index
 #  fi
-#  if [ -e tomcat/webapps/ROOT/index-data ]; then 
+#  if [ -e tomcat/webapps/ROOT/index-data ]; then
 #   rm -r tomcat/webapps/ROOT/index-data
 #  fi
-#  rm -r heritrix-3.2.0/jobs/* 
+#  rm -r heritrix-3.2.0/jobs/*
 #  rm tomcat/webapps/ROOT/files1/*
 #fi
+
+
+DIRECTORY="/Applications/WAIL.app/"
+
+# Check if WAIL.app exists. Ask the user whether to nuke old binary. Exit if 'no'
+if [ -d "$DIRECTORY" ]; then
+  echo $DIRECTORY" already exists!"
+  while true; do
+    read -p "Do you want me to delete the old app and continue (y/n)?" yn
+    case $yn in
+        [Yy]* ) echo "Continuing to build"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+  done
+fi
+
 
 #arch -i386 python ../build/pyinstaller-2.0/pyinstaller.py WAIL.py --onefile --windowed
 # Above no longer works on OS X>10.8.5 though it might be a homebrew vs. Apple python issue.
@@ -31,6 +48,7 @@ sed -i "" "s/0.0.0/$todaysVersion by Mat Kelly/g" "./dist/WAIL.app/Contents/Info
 
 # Copy the bundledApps and support directories to inside WAIL.app/
 cp -r ./bundledApps ./support ./build ./dist/WAIL.app/
+rm -rf /Applications/WAIL.app
 mv ./dist/WAIL.app /Applications/
 mv ./dist/WAIL /Applications/WAILX
 
@@ -42,4 +60,3 @@ rm -r ./build/WAIL
 #rm -r dist
 #rm WAIL.spec
 #rm -r build
-
