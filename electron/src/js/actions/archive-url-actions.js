0139428c5c28a85867777c3368c42d81c49d6dbd
N0taN3rd <n0tan3rd@gmail.com>
2016-06-02 00:15:30 -0400

    Noticed I had repeated the urlUpdated logic in askMemgator in archive-url-actions.

--- electron/src/js/actions/archive-url-actions.js
+++ electron/src/js/actions/archive-url-actions.js
@@ -1,6 +1,6 @@
 import UrlDispatcher from '../dispatchers/url-dispatcher'
 import wailConstants from '../constants/wail-constants'
-import shelljs from 'shelljs'
+import child_process from 'child_process'
 import UrlStore from '../stores/urlStore'
 
 const EventTypes = wailConstants.EventTypes
@@ -16,12 +16,12 @@ export function urlUpdated(url) {
 
 export async function askMemgator(url) {
     console.log('askingMemegator')
-    shelljs.exec(`${Paths.memgator} -a ${Paths.archives} ${url}`,(code,output,err) => {
-        console.log(code,output,err)
-        console.log((output.match(/memento/g) || []).length)
+    child_process.exec(`${Paths.memgator} -a ${Paths.archives} ${url}`, (err, stdout, stderr) => {
+        console.log(err, stdout, stderr)
+        console.log((stdout.match(/memento/g) || []).length)
         UrlDispatcher.dispatch({
-            type: EventTypes.HAS_VAILD_URI,
-            url: url
+            type: EventTypes.GOT_MEMENTO_COUNT,
+            mementos: url
         })
     })
     
