4422cca85b554dbe1a6aedfa17838738c366d12f
John Berlin <n0tan3rd@gmail.com>
2016-07-06 21:45:14 -0400

    I keep forgetting

--- src/dispatchers/crawl-dispatcher.js
+++ src/dispatchers/crawl-dispatcher.js
@@ -1,7 +1,8 @@
-import Dispatcher from 'flux'
+import { Dispatcher } from 'flux'
 
 //use of a variable here allows us to attach our dispatchers to the window
 //if desired
 const CrawlDispatcher = new Dispatcher()
+CrawlDispatcher.dispatch()
 
 export default CrawlDispatcher
