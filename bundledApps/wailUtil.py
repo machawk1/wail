0ef8e1784b49beafe4acc6919c95179416152c52
Mat Kelly <me@matkelly.com>
2018-02-05 20:58:09 -0500

    Re-add imports to util file.

---
+++ bundledApps/wailUtil.py
@@ -0,0 +1,38 @@
+import shutil
+import errno
+import os
+
+
+def tail(filename, lines=1, _buffer=4098):
+    try:
+        f = open(filename, "r")
+    except IOError as e:
+        return '{0}\n{1}'.format(
+            "No job info yet",
+            "You must run a job before stats can be shown here")
+    lines_found = []
+    block_counter = -1
+    while len(lines_found) < lines:
+        try:
+            f.seek(block_counter * _buffer, os.SEEK_END)
+        except IOError:
+            # either file is too small, or too many lines requested
+            f.seek(0)
+            lines_found = f.readlines()
+            break
+
+        lines_found = f.readlines()
+        if len(lines_found) > lines:
+            break
+        block_counter -= 1
+    return lines_found[-lines:]
+
+
+def copyanything(src, dst):
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc:  # python >2.5
+        if exc.errno == errno.ENOTDIR:
+            shutil.copy(src, dst)
+        else:
+            raise
