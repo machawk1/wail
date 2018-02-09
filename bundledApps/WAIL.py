1cab30236fddeb87ca966bfcfcb306c9d733ae4a
Mat Kelly <me@matkelly.com>
2018-02-09 14:31:52 -0500

    Add memento count delimiter for #308

--- bundledApps/WAIL.py
+++ bundledApps/WAIL.py
@@ -16,6 +16,7 @@ import os
 import schedule
 import time
 import sys
+import locale
 import datetime
 # from ntfy.backends.default import notify
 
@@ -192,6 +193,8 @@ class WAILGUIFrame_Basic(wx.Panel):
 
         memCountMsg = ''
         if mCount > 0:
+            locale.setlocale(locale.LC_ALL, 'en_US')
+            mCount = locale.format("%d", mCount, grouping=True)
             mPlurality = 's'
             aPlurality = 's'
             if mCount == 1:
