ef62f98219925f55a5f4d5b7771174e0b9a2ef47
Mat Kelly <me@matkelly.com>
2018-11-29 11:05:33 -0500

    Install dependencies on Windows

--- bundledApps/MAKEFILE.bat
+++ bundledApps/MAKEFILE.bat
@@ -1,3 +1,6 @@
+pip install --upgrade wxPython
+pip install -r requirements.txt
+
 pyinstaller --onefile --windowed --version-file=build/version.txt --icon=build/icons/wail_blue.ico bundledApps/WAIL.py
 
 ::Windows shell (CMD.exe)
