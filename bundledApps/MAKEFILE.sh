2dc06aa8d96bfe420e64531ee9dccd7255bcd176
Mat Kelly <me@matkelly.com>
2022-08-26 17:05:32 -0400

    Add fetch of remote modules file for JDK

--- bundledApps/MAKEFILE.sh
+++ bundledApps/MAKEFILE.sh
@@ -9,6 +9,10 @@ trap exit INT
 
 main ()
 {
+  # Fetch modules file for JDK, which is too big for GitHub
+  echo "Fetching remove JDK modules file, which is too large to store on GitHub"
+  curl https://matkelly.com/temp/temurin-17.jdk/Contents/Home/lib/modules --output ./bundledApps/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/lib/modules
+
   # Provide a means to skip all questions for GitHub Actions
   while getopts ":q" opt; do
     case ${opt} in
